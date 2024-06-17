8c1cd138b2da2a419bf49088959f89708a4cc873
${sleep,7} <52643283+mrthankyou@users.noreply.github.com>
2024-06-17 11:30:51 -0400

    Add additional math functions

--- src/libraries/math.py
+++ src/libraries/math.py
@@ -1,7 +1,27 @@
 # Returns common subtraction functions
 def math_subtraction_function_names():
   return [
-    "safeSub", # safeSub(2,1) // Custom made func
-    "sub",      # 2.sub(1, "revert message") // SafeMath. Can also be sub(2,1,"revert message")
-    "subtract"  # subtract(2, 1) // OZ
+    "safeSub",  # Custom func: safeSub(2,1)
+    "sub",      # Safemath: 2.sub(1, "revert message") Can also be sub(2,1,"revert message")
+    "subtract"  # Custom func: subtract(2, 1) 
+  ]
+
+# Returns common addition functions
+def math_addition_function_names():
+  return [
+    "add",     # Safemath: add(uint256 a, uint256 b)
+    "safeAdd", # Custom func: safeAdd(uint a, uint b)
+  ]
+
+# Returns common multiplication functions
+def math_multiply_function_names():
+  return [
+    "mul",    # Safemath: mul(uint256 a, uint256 b)
+    "safeMul" # Custom func: safeMul(uint a, uint b)
+  ]
+
+# Returns common division functions
+def math_division_function_names():
+  return [
+    "div", # Safemath: div(uint256 a, uint256 b) or div(a, b, errorMessage)
   ]
